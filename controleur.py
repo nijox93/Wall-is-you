e29a860bcff9fccdc596de67140b199c7d74e221
nijox93 <128825568+nijox93@users.noreply.github.com>
2023-04-11 10:08:43 +0200

    Update controleur.py

--- controleur.py
+++ controleur.py
@@ -1,4 +1,5 @@
 from modele import*
+from vue import*
 
 #INITIALISATION
 f = "game_files/maps/map_test.txt"
@@ -6,3 +7,5 @@ f = "game_files/maps/map_test.txt"
 donjon, position, dragons = charge_fichier(f)
 pos_dragons = position_dragons(dragons)
 aventurier = Aventurier(position, 1)
+print(intention(donjon, position, pos_dragons))
+dessine_donjon(donjon, dragons, aventurier)
