1a2462bcfcaabe35d306708b1d1126bc1a9ca924
nijox93 <128825568+nijox93@users.noreply.github.com>
2023-04-18 10:05:55 +0200

    Update controleur.py

--- controleur.py
+++ controleur.py
@@ -9,3 +9,14 @@ pos_dragons = position_dragons(dragons)
 aventurier = Aventurier(position, 1)
 print(intention(donjon, position, pos_dragons))
 dessine_donjon(donjon, dragons, aventurier)
+while(True):
+    attend_ev()
+    i, j = 2, 1
+#     pivoter(donjon, (i, j))
+#     dessine_portes(donjon, (i, j))
+    for i in range(5):
+        for j in range(2):
+            pivoter(donjon, (i, j))
+            dessine_portes(donjon, (i, j))
+    print(donjon[2][1])
+    
