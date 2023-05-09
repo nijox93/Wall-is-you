6befe2177db9ea07aa55c59fc57a7b7bff2f3100
nijox93 <128825568+nijox93@users.noreply.github.com>
2023-05-09 08:19:47 +0200

    Update controleur.py

--- controleur.py
+++ controleur.py
@@ -7,16 +7,27 @@ f = "game_files/maps/map_test.txt"
 donjon, position, dragons = charge_fichier(f)
 pos_dragons = position_dragons(dragons)
 aventurier = Aventurier(position, 1)
-print(intention(donjon, position, pos_dragons))
+chemin = intention(donjon, position, pos_dragons)
+
 dessine_donjon(donjon, dragons, aventurier)
+dessine_aventurier(donjon, aventurier.position)
+dessine_chemin(donjon, chemin)
+for pos in pos_dragons:
+    dessine_dragon(donjon, pos)
+
 while(True):
-    attend_ev()
-    i, j = 2, 1
-#     pivoter(donjon, (i, j))
-#     dessine_portes(donjon, (i, j))
-    for i in range(5):
-        for j in range(2):
-            pivoter(donjon, (i, j))
-            dessine_portes(donjon, (i, j))
-    print(donjon[2][1])
-    
+    ev = donne_ev()
+    tev = type_ev(ev)
+    if tev == "Quitte":
+        break
+    elif tev == "ClicGauche":
+        for i in range(len(donjon)):
+            for j in range(len(donjon)):
+                pivoter(donjon, (i, j))
+                dessine_portes(donjon, (i, j))
+    elif tev == "ClicDroit":
+        chemin = intention(donjon, position, pos_dragons)
+        print(chemin)
+        dessine_chemin(donjon, chemin)
+    mise_a_jour()
+ferme_fenetre()
