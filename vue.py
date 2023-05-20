bb6c560aa9b481429bfd7fb801699d63fca80e84
nijox93 <128825568+nijox93@users.noreply.github.com>
2023-05-20 14:31:54 +0200

    Update vue.py

--- vue.py
+++ vue.py
@@ -11,6 +11,7 @@ cree_fenetre(largeur, hauteur)
 rectangle(0, 0, largeur, hauteur, remplissage="white")
 # image(largeur//2, hauteur//2, 'game_files/media/fond.png', hauteur=1500, largeur=2000, tag="fond")
 
+
 def dessine_donjon(donjon, dragons, aventurier):
     # dragons et aventurier sont ici compris comme des coordonnées
     for i in range(len(donjon)):
@@ -36,7 +37,8 @@ def dessine_donjon(donjon, dragons, aventurier):
                     elif n == 3:
                         rectangle(x0, y1-k, x0+k, y0+k, couleur="white", remplissage="white", tag="p3"+str(i)+str(j))
                 n += 1
-    
+
+
 def dessine_portes(donjon, position):
     (i,j) = position
     x0, y0 = j * (largeur/len(donjon)), i * (hauteur/len(donjon[0]))
@@ -57,21 +59,26 @@ def dessine_portes(donjon, position):
             efface("p"+str(n)+str(i)+str(j))
         n += 1
 
+
 def dessine_image(donjon, position, path, tag):
     (i,j) = position
     x0, y0 = j * (largeur/len(donjon)), i * (hauteur/len(donjon[0]))
     x1, y1 = (j+1) * (largeur/len(donjon)), (i+1) * (hauteur/len(donjon[0]))
     image(x0 + (x1 - x0)//2, y0 + (y1 - y0)//2, path, hauteur=int(k*2), largeur=int(k*2), tag=tag)
 
+
 def dessine_aventurier(donjon, position):
     efface("aventurier")
     dessine_image(donjon, position, "game_files/media/pikachu.png", "aventurier")
 
-def dessine_dragons(donjon, pos_dragons):
+
+def dessine_dragons(donjon, dragons):
     ''' Dessine des dragons '''
     efface("dragon")
-    for position in pos_dragons:
+    for dragon in dragons:
+        position = dragon.position
         dessine_image(donjon, position, "game_files/media/gengar2.png", "dragon")
+
 
 def dessine_chemin(donjon, chemin):
     efface("chemin")
@@ -91,6 +98,14 @@ def dessine_chemin(donjon, chemin):
             ligne(xf_1, yf_1, xf_2, yf_2, 'red', k/3, tag="chemin")
             n += 1
 
+
 def donne_position(donjon, abscisse, ordonnee):
     ''' Renvoie la case correspondant au couple de coordonnées abscisse/ordonnée '''
     return int(abscisse // (largeur/len(donjon[0]))), int(ordonnee // (hauteur/len(donjon)))
+
+
+def dessine_niveau(donjon, dragons, aventurier):
+    ''' Dessine tous les composants d'un niveau '''
+    dessine_donjon(donjon, dragons, aventurier)
+    dessine_aventurier(donjon, aventurier.position)
+    dessine_dragons(donjon, dragons)
